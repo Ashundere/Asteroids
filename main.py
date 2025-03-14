543957e63be24c7573544244213518a56f11ccc2
Ashundere <devon.shun.jones@gmail.com>
2025-03-14 05:38:26 -0500

    Create asteroid splitting behavior

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
 		for asteroid in asteroids:
 			for shot in shots:
 				if asteroid.collides_with(shot):
-					asteroid.kill()
+					asteroid.split()
 					shot.kill()
 		pygame.display.flip()
 		clock.tick(60)
