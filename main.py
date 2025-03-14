3dd7cf94dd12384e0f8351ccae25a1b1ee7b0ba1
Ashundere <devon.shun.jones@gmail.com>
2025-03-14 03:28:04 -0500

    Add player movement functionality

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+
+
 
 def main():
 	pygame.init()
@@ -10,12 +13,19 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	player = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2),PLAYER_RADIUS)
+	dt = 0
 	while pygame.get_init() == 1:
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
        				 return
 		pygame.Surface.fill(screen,"black")
+		player.draw(screen)
+		player.update(dt)
 		pygame.display.flip()
-
+		clock.tick(60)
+		dt = (clock.tick(60) / 1000)
+		
 if __name__ == "__main__":
     main()
